173b7ed4daf8c17bbd218a4b02d23a727da1ddd3
luqmaan-onsight <138872917+luqmaan-onsight@users.noreply.github.com>
2024-04-21 10:48:04 +0200

    update app domain

--- frontend/src/utils/WebStore.js
+++ frontend/src/utils/WebStore.js
@@ -1,5 +1,5 @@
 export const webStore = {
     APP_URL: "http://mycontabovps.site:8080",
     //APP_URL: "http://localhost:8080",
-    APP_DOMAIN: "uberlink.online"
+    APP_DOMAIN: "uberlink.netlify.app"
 }
